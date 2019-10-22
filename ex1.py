ac32dd7a40f4bf8c6b891c589bf1e4628f9f7c29
jilesingh <itsjilesingh@gmail.com>
2019-10-22 23:28:37 +0200

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# -*- coding utf-8 -*-
+
+print ("Hello World!")
+print ("Hello again!")
+print ("I like typing this.")
+print ("This is fun.")
+print ("Yay1 Printing.")
+print ("I'd much rather you not.")
+print ('I "said" do not touch this.')
+print ("I want to push my project to git.")
