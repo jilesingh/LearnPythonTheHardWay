9e7abff07d31ac1b6af8b9b176695e1da03da873
jilesingh <itsjilesingh@gmail.com>
2019-10-25 16:38:18 +0200

    new_branch

---
+++ chapter2/ex2.py
@@ -0,0 +1,18 @@
+# -*- coding utf-8 -*-
+# This code is written and run using Python Version 3.6.8
+
+# A comment, this is so you can read your program later
+# anything after # is ignored by python
+
+print ("I could have code like this.")  # and the comment after this is ignored
+
+
+# You can also use a comment to "disable" or comment out a pice of code
+
+# print ("This won't work")
+
+print("This will run.")
+
+# Here is the output as observed from the terminal:
+'''I could have code like this.
+This will run.'''
