2a5a728acc6485f8902871c15343c1e6b75cbaef
jilesingh <itsjilesingh@gmail.com>
2019-10-26 12:20:16 +0200

    all

---
+++ chapter1/ex32.py
@@ -0,0 +1,57 @@
+# -*- coding utf-8 -*-
+# This code is written and run using Python Version 3.6.8
+
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+for number in the_count:  # for loop is iterating through a list
+    print ("This is count %d" % number)
+
+for fruit in fruits:
+    print ("A fruit of type: %s" % fruit)
+
+for i in change:
+    print ("I got %r" % i) # when we don't know the data type we use %r
+
+# here we are going to use empty list and later we extend it
+
+element = []
+
+for i in range(0, 6):
+    print ("Adding %d to the list." %i)
+    element.append(i)  # append is a function used to append a list.
+
+for i in element:
+    print ("Element was: %d" % i)
+
+# Here is the output as observed from the terminal
+'''
+This is count 1
+This is count 2
+This is count 3
+This is count 4
+This is count 5
+A fruit of type: apples
+A fruit of type: oranges
+A fruit of type: pears
+A fruit of type: apricots
+I got 1
+I got 'pennies'
+I got 2
+I got 'dimes'
+I got 3
+I got 'quarters'
+Adding 0 to the list.
+Adding 1 to the list.
+Adding 2 to the list.
+Adding 3 to the list.
+Adding 4 to the list.
+Adding 5 to the list.
+Element was: 0
+Element was: 1
+Element was: 2
+Element was: 3
+Element was: 4
+Element was: 5
+'''
---
+++ chapter1/ex33.py
@@ -0,0 +1,48 @@
+# -*- coding utf-8 -*-
+# This code is written and run using Python Version 3.6.8
+
+i = 0
+numbers = []
+
+while i < 6:
+    print ("At the top i is %d" %i)
+    numbers.append(i)
+
+    i = i + 1
+    print ("Numbers now:", numbers)
+    print ("At the bottom i is %d" %i)
+
+print ("The numbers: ")
+
+for num in numbers:
+    print (num)
+
+
+# Here is output as observed from the terminal.
+'''
+At the top i is 0
+Numbers now: [0]
+At the bottom i is 1
+At the top i is 1
+Numbers now: [0, 1]
+At the bottom i is 2
+At the top i is 2
+Numbers now: [0, 1, 2]
+At the bottom i is 3
+At the top i is 3
+Numbers now: [0, 1, 2, 3]
+At the bottom i is 4
+At the top i is 4
+Numbers now: [0, 1, 2, 3, 4]
+At the bottom i is 5
+At the top i is 5
+Numbers now: [0, 1, 2, 3, 4, 5]
+At the bottom i is 6
+The numbers: 
+0
+1
+2
+3
+4
+5
+'''
