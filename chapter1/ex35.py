81de27ab220ebcde9be3c80fcb1cf05067f453a0
jilesingh <itsjilesingh@gmail.com>
2019-10-26 19:34:12 +0200

    Branches and Functions

--- chapter1/ex35.py
+++ chapter1/ex35.py
@@ -9,12 +9,14 @@ def gold_room():
     next = input(">")
     if "0" or "1" in next:
         how_much = int(next)
+
     else:
         dead("Man, learn to type a number")
 
     if how_much < 50:
         print ("Nice, you are not greedy, you win!")
         exit(0)
+
     else:
         dead("You greedy bastard!")
 
@@ -31,13 +33,17 @@ def bear_room():
 
         if next == "take honey":
             dead("The bear looks at you then slaps your face off.")
+
         elif next == "taunt bear" and not bear_moved:
             print ("The bear has moved from the door. You can go through it now.")
             bear_moved = True
+
         elif next == "taunt bear" and bear_moved:
             dead("The bear gets pissed off and chews your leg off.")
+
         elif next == "open door" and bear_moved:
             gold_room()
+            
         else:
             print("I got no idea what that means.")
 
@@ -50,8 +56,10 @@ def cthulhu_room():
 
     if "flee" in next:
         start()
+
     elif "head" in next:
         dead ("Well that was tasty!")
+
     else:
         cthulhu_room()
 
@@ -68,8 +76,10 @@ def start():
 
     if next == "left":
         bear_room()
+
     elif next == "right":
         cthulhu_room()
+
     else:
         dead("You stumble around the room until you starve.")
 
